d770873302c7ef24c11de533e8a86a5212a46da5
Diljot Garcha <diljotsg@gmail.com>
2020-04-21 13:05:03 -0500

    Updates

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,6 +5,11 @@ mkdir ~/Developer
 mkdir ~/Developer/Git
 mkdir ~/.ssh
 
+# Remove existing files (if they exist)
+rm ~/.zshrc
+rm ~/.ssh/config
+rm ~/.gitconfig
+
 # Link config files
 ln -s ~/.afterlife/dotfiles/.zshrc ~/.zshrc
 ln -s ~/.afterlife/dotfiles/.ssh_config ~/.ssh/config
